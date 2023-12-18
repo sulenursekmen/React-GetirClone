55bcbf8afb42740ffa3649a358c67863da436945
sulenursekmen <sulenursekmenn@gmail.com>
2023-12-19 01:06:30 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-color': '#5d2ebc',
+      },
+    },
+  },
+  plugins: [],
+}
+
